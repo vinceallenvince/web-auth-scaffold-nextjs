7598a810812b542392730682e81e3ceb11f0c697
Vince Allen <vince@spotify.com>
2025-04-28 13:44:28 -0400

    Refactors the SVG icons in the toast component

--- src/app/components/ui/toast/toast.tsx
+++ src/app/components/ui/toast/toast.tsx
@@ -25,21 +25,18 @@ export function Toast({ message, type, onDismiss }: ToastProps) {
     >
       <div className="flex justify-between w-full">
         <div className="flex items-center">
-          {type === "success" && (
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+          {/* Map toast type to appropriate SVG path */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+            {type === "success" && (
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          )}
-          {type === "error" && (
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+            )}
+            {type === "error" && (
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          )}
-          {type === "info" && (
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+            )}
+            {type === "info" && (
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          )}
+            )}
+          </svg>
           <span>{message}</span>
         </div>
         <button
