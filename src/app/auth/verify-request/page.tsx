041ea9df86a17c5fcddbf3e8b20935cd6f681b20
Vince Allen <vince@spotify.com>
2025-04-29 10:52:31 -0400

    Replaces fieldset with card

--- src/app/auth/verify-request/page.tsx
+++ src/app/auth/verify-request/page.tsx
@@ -11,10 +11,9 @@ export default function VerifyRequestPage() {
           <Container>
             <div className="flex flex-col lg:flex-row gap-8">
               <div className="w-full">
-                <div className="max-w-md">
-                  <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    
-                    <div className="card-body p-6">
+              <div className="card card-border border-base-300 bg-base-100 w-96">
+
+                    <div className="card-body p-4">
                       <h1 className="card-title text-xl font-bold">Magic Link Sent!</h1>
                       <p className="mt-2 text-base-content/70">
                         Check your email for the magic link. If you don&apos;t see it, check your spam folder or try a different email.
@@ -29,14 +28,12 @@ export default function VerifyRequestPage() {
                         </Link>
                       </div>
                     </div>
-                  </fieldset>
+                 
                 </div>
               </div>
 
-              <div className="card card-border bg-base-100 w-full">
-               <div className="card-body">
+              <div className="w-full">
                   &nbsp;
-                </div>
               </div>
               <div className="w-full">
                 &nbsp;
